aac97392d030b816561f8d0a881c5fd12918adfe
Fitri Kusuma Wardani <fitrikusumaktp45@gmail.com>
2022-09-01 19:42:37 +0700

    Initial commit

---
+++ system/resources/views/detail-product.blade.php
@@ -0,0 +1,134 @@
+@include('navbar')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>aranoz</title>
+    
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>aranoz</title>
+  <link rel="icon" href="public/img/favicon.png">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="public/css/bootstrap.min.css">
+  <!-- animate CSS -->
+  <link rel="stylesheet" href="public/css/animate.css">
+  <!-- owl carousel CSS -->
+  <link rel="stylesheet" href="public/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="public/css/lightslider.min.css">
+  <!-- font awesome CSS -->
+  <link rel="stylesheet" href="public/css/all.css">
+  <!-- flaticon CSS -->
+  <link rel="stylesheet" href="public/css/flaticon.css">
+  <link rel="stylesheet" href="public/css/themify-icons.css">
+  <!-- font awesome CSS -->
+  <link rel="stylesheet" href="public/css/magnific-popup.css">
+  <!-- style CSS -->
+  <link rel="stylesheet" href="public/css/style.css">
+  
+</head>
+
+<body>
+
+  <!--================Single Product Area =================-->
+  <div class="product_image_area section_padding">
+    <div class="container">
+      <div class="row s_product_inner justify-content-between">
+        <div class="col-lg-7 col-xl-7">
+          <div class="product_slider_img">
+            <div id="vertical">
+              <div data-thumb="public/img/product/single-product/balenciaga-bb0117o_1.png">
+                <img src="public/img/product/single-product/balenciaga-bb0117o_1.png" />
+              </div>
+              <div data-thumb="public/img/product/single-product/balenciaga-bb0117o-2.png">
+                <img src="public/img/product/single-product/balenciaga-bb0117o-2.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-5 col-xl-4">
+          <div class="s_product_text">
+            <h3>Balenciaga BB0117O</h3>
+            <h2>Rp3.269.514</h2>
+            <ul class="list">
+              <li>
+                <a><span>Made in</span> : Italy</a>
+              </li>
+              <li>
+                <a> <span>Availibility</span> : In Stock</a>
+              </li>
+            </ul>
+            <p>
+              Menunjukkan kasih sayang di depan umum adalah hal yang luar biasa. 
+              Jadi tunjukkan kecintaan Anda pada kacamata logo pilot dari Balenciaga Eyewear ini.
+            </p>
+            <div class="card_area d-flex justify-content-center align-items-center">
+              
+              <a href="{{url('/login')}}" class="btn_3">add to cart</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--================End Single Product Area =================-->
+
+  <!--::footer_part start::-->
+  <footer class="footer_part">
+    <div class="container">
+      <div class="row justify-content-around">
+     
+      </div>
+    </div>
+    <div class="copyright_part">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="copyright_text">
+              <P>
+                Copyright &copy;Fitri Kusuma W</a>
+              </P>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!--::footer_part end::-->
+
+  <!-- jquery plugins here-->
+  <!-- jquery -->
+  <script src="public/js/jquery-1.12.1.min.js"></script>
+  <!-- popper js -->
+  <script src="public/js/popper.min.js"></script>
+  <!-- bootstrap js -->
+  <script src="public/js/bootstrap.min.js"></script>
+  <!-- easing js -->
+  <script src="public/js/jquery.magnific-popup.js"></script>
+  <!-- swiper js -->
+  <script src="public/js/lightslider.min.js"></script>
+  <!-- swiper js -->
+  <script src="public/js/masonry.pkgd.js"></script>
+  <!-- particles js -->
+  <script src="public/js/owl.carousel.min.js"></script>
+  <script src="public/js/jquery.nice-select.min.js"></script>
+  <!-- slick js -->
+  <script src="public/js/slick.min.js"></script>
+  <script src="public/js/swiper.jquery.js"></script>
+  <script src="public/js/jquery.counterup.min.js"></script>
+  <script src="public/js/waypoints.min.js"></script>
+  <script src="public/js/contact.js"></script>
+  <script src="public/js/jquery.ajaxchimp.min.js"></script>
+  <script src="public/js/jquery.form.js"></script>
+  <script src="public/js/jquery.validate.min.js"></script>
+  <script src="public/js/mail-script.js"></script>
+  <script src="public/js/stellar.js"></script>
+  <!-- custom js -->
+  <script src="public/js/theme.js"></script>
+  <script src="public/js/custom.js"></script>
+</body>
+
+</html>
